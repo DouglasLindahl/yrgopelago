288dcf77efd028113e652813103f6342289bb07c
DouglasLindahl <douglas.lindahl@gmail.com>
2022-12-19 22:02:08 +0100

    money should be added to my account

--- paymentVerification.php
+++ paymentVerification.php
@@ -20,18 +20,22 @@ $response = $client->request('POST', 'https://www.yrgopelago.se/centralbank/with
 ]);
 $payment = $response->getBody()->getContents();
 $payment = json_decode($payment, true);
+
+
 $_SESSION["transfer_code"] = $payment["transferCode"];
 
 //add money to my account---------------
 
 $response = $client->request('POST', 'https://www.yrgopelago.se/centralbank/deposit', [
     'form_params' => [
-        "user" => "Douglas", "transfercode" => $payment["transferCode"]
+        "user" => "Douglas", "transferCode" => $payment["transferCode"]
     ]
 ]);
+
 $response = $response->getBody()->getContents();
 $response = json_decode($response, true);
+echo "<pre>";
 var_dump($response);
 
 $_SESSION["payment_passed"] = true;
-//header("location:index.php");
+header("location:index.php");

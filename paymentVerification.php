3e38424e55d9718a501e5b514112921cbb124232
DouglasLindahl <douglas.lindahl@gmail.com>
2022-12-30 01:56:08 +0100

    users can no longer book rooms with fake information

--- paymentVerification.php
+++ paymentVerification.php
@@ -28,72 +28,77 @@ $payment = json_decode($payment, true);
 
 // Check if the payment array contains an "error" key
 if (array_key_exists("error", $payment)) {
+    echo $payment["error"];
     // Redirect to the index page
-    header("index.php");
+    header("location:index.php");
+} else {
+    // Set the session variable "transfer_code" to the transfer code in the payment array
+    $_SESSION["transfer_code"] = $payment["transferCode"];
+
+    // Make a POST request to the specified URL, with the user name and transfer code as form parameters
+    $response = $client->request('POST', 'https://www.yrgopelago.se/centralbank/deposit', [
+        'form_params' => [
+            "user" => "Douglas", "transferCode" => $payment["transferCode"]
+        ]
+    ]);
+    $deposit = $response->getBody()->getContents();
+    if (!strpos($deposit, "Success.")) {
+        header("location:index.php");
+        die();
+    }
+    // Get the response body as a string and decode it into a PHP array
+    $response = $response->getBody()->getContents();
+    $response = json_decode($response, true);
+
+    // Set the session variable "booking" to an array with the user name
+    $_SESSION["booking"]["name"] = $_POST["name"];
+
+    // Set the session variable "payment_passed" to true
+    $_SESSION["payment_passed"] = true;
+
+
+    // Prepare an INSERT statement to insert a new record into the "guests" table
+    $stmt = $database->prepare("INSERT INTO guests ('room_id', 'name', 'arrival_date', 'departure_date', 'total_cost')  values(?, ?, ?, ?, ?)");
+
+    // Bind the session variables to the statement parameters
+    $stmt->bindParam(1, $_SESSION["booking"]["room"]);
+    $stmt->bindParam(2, $_SESSION["booking"]["name"]);
+    $stmt->bindParam(3, $_SESSION["booking"]["arrivalDate"]);
+    $stmt->bindParam(4, $_SESSION["booking"]["departureDate"]);
+    $stmt->bindParam(5, $_SESSION["booking"]["cost"]);
+    // Execute the statement
+    $stmt->execute();
+
+    // Prepare a SELECT statement to fetch all records from the "guests" table
+    $stmt = $database->prepare("SELECT * from guests");
+    // Execute the statement
+    $stmt->execute();
+    // Fetch the result as an associative array
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Initialize an empty array
+    $datePeriod = [];
+
+    // Iterate over the result array
+    foreach ($result as $guest) {
+        // Extract the day of the month from the arrival and departure dates
+        $datePeriod["arrival_date"] = $arrivalDate = date("d", intval(strtotime($guest["arrival_date"])));
+        $datePeriod["departure_date"] = $departureDate = date("d", intval(strtotime($guest["departure_date"])));
+        // Set the "room" element to the session variable "room"
+        $datePeriod["room"] = $room = $_SESSION["booking"]["room"];
+    }
+
+    // Read the contents of the "guests.json" file
+    $guests = file_get_contents("guests.json");
+    // Decode the JSON string into a PHP array
+    $guests = json_decode($guests);
+    // Add the date period array to the "guests" element of the array
+    array_push($guests->{'guests'}, $datePeriod);
+    // Encode the array back into a JSON string
+    $guests = json_encode($guests);
+    // Write the JSON string back to the "guests.json" file
+    file_put_contents("guests.json", $guests);
+
+    // Redirect to the confirmation page
+    header("location:confirmation.php");
 }
-
-// Set the session variable "transfer_code" to the transfer code in the payment array
-$_SESSION["transfer_code"] = $payment["transferCode"];
-
-// Make a POST request to the specified URL, with the user name and transfer code as form parameters
-$response = $client->request('POST', 'https://www.yrgopelago.se/centralbank/deposit', [
-    'form_params' => [
-        "user" => "Douglas", "transferCode" => $payment["transferCode"]
-    ]
-]);
-
-// Get the response body as a string and decode it into a PHP array
-$response = $response->getBody()->getContents();
-$response = json_decode($response, true);
-
-// Set the session variable "booking" to an array with the user name
-$_SESSION["booking"]["name"] = $_POST["name"];
-
-// Set the session variable "payment_passed" to true
-$_SESSION["payment_passed"] = true;
-
-
-// Prepare an INSERT statement to insert a new record into the "guests" table
-$stmt = $database->prepare("INSERT INTO guests ('room_id', 'name', 'arrival_date', 'departure_date', 'total_cost')  values(?, ?, ?, ?, ?)");
-
-// Bind the session variables to the statement parameters
-$stmt->bindParam(1, $_SESSION["booking"]["room"]);
-$stmt->bindParam(2, $_SESSION["booking"]["name"]);
-$stmt->bindParam(3, $_SESSION["booking"]["arrivalDate"]);
-$stmt->bindParam(4, $_SESSION["booking"]["departureDate"]);
-$stmt->bindParam(5, $_SESSION["booking"]["cost"]);
-// Execute the statement
-$stmt->execute();
-
-// Prepare a SELECT statement to fetch all records from the "guests" table
-$stmt = $database->prepare("SELECT * from guests");
-// Execute the statement
-$stmt->execute();
-// Fetch the result as an associative array
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// Initialize an empty array
-$datePeriod = [];
-
-// Iterate over the result array
-foreach ($result as $guest) {
-    // Extract the day of the month from the arrival and departure dates
-    $datePeriod["arrival_date"] = $arrivalDate = date("d", strtotime($guest["arrival_date"]));
-    $datePeriod["departure_date"] = $departureDate = date("d", strtotime($guest["departure_date"]));
-    // Set the "room" element to the session variable "room"
-    $datePeriod["room"] = $room = $_SESSION["booking"]["room"];
-}
-
-// Read the contents of the "guests.json" file
-$guests = file_get_contents("guests.json");
-// Decode the JSON string into a PHP array
-$guests = json_decode($guests);
-// Add the date period array to the "guests" element of the array
-array_push($guests->{'guests'}, $datePeriod);
-// Encode the array back into a JSON string
-$guests = json_encode($guests);
-// Write the JSON string back to the "guests.json" file
-file_put_contents("guests.json", $guests);
-
-// Redirect to the confirmation page
-header("location:confirmation.php");
